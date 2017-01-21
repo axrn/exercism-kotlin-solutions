a04d96cf904e9c462c9c72f456bd00e73bcbc89f
alex_r <blank.basic@gmail.com>
2017-01-21 21:23:38 +0300

    Added exercise "Gigasecond"
    http://exercism.io/exercises/kotlin/gigasecond/readme

---
+++ gigasecond/src/main/kotlin/Gigasecond.kt
@@ -0,0 +1,7 @@
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+class Gigasecond (dateTime: LocalDateTime) {
+    constructor(date: LocalDate) : this(date.atStartOfDay())
+    val date = dateTime.plusSeconds(1e9.toLong())
+}
