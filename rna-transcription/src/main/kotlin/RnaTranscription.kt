1bc940f2e145b0a0e5898dcee183f27074599f60
alex_r <blank.basic@gmail.com>
2017-01-27 20:34:34 +0300

    Removed old version

--- rna-transcription/src/main/kotlin/RnaTranscription.kt
+++
@@ -1,9 +0,0 @@
-fun transcribeToRna(s: String): String {
-        fun inner(s2: Char) = when (s2) {
-            'C' -> 'G'
-            'G' -> 'C'
-            'T' -> 'A'
-            'A' -> 'U'
-            else -> throw IllegalArgumentException() }
-        return s.map{ inner(it) }.joinToString("")
-    }
